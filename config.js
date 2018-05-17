2c9d081881bb3feb260766ea2880c32dce8858df
Umran Hussein <umran.hussein@gmail.com>
2018-05-17 00:10:24 -0700

    changed database address to mlab address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   cookie_secret: 'cookie secret',
-  mongo_url: 'mongodb://bathbuilt:bathbuilt@ds047802.mlab.com:47802/heroku_lttkwmbv',
+  mongo_url: 'mongodb://bathbuilt:bathbuilt@ds119406.mlab.com:19406/heroku_qmx64zg9',
   contentful: {
     space: '3qgfejllni6q',
     access_token: 'ab503d6af078ad40f98721798667cccc1f3ebc8dea68ccd0a57a4845670041f4'
