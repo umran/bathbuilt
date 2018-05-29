b4444b711a3ab1e797bf32e5aeca04a9047ea153
Umran Hussein <umran.hussein@gmail.com>
2018-05-29 04:32:56 -0700

    fixed javascript bug

--- public/javascripts/bathbuilt.js
+++ public/javascripts/bathbuilt.js
@@ -332,7 +332,7 @@ function getPrice() {
     url: "/get-price",
     data: formData,
     success: function(data){
-      if(data.status == 'success') {
+      if(data.status == 'success' && data.response.entry) {
         // change price
         $('#price').text('$' + data.response.entry.fields.price['en-US'])
       } else {
